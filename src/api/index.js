8f8e2b598bb48417129ae69f80731ec6e3bd90cc
aashlesha chitte <stsd21769ac@M-C02CQ28DMD6P.local>
2025-03-24 16:05:34 +0530

    changed port

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:9000/api";
+const API_BASE_URL = "http://localhost:9001/api";
 
 const getToken = () => {
   return localStorage.getItem("token");
